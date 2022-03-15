f29e69f7f51599d56203888ecc97e641b2c137c3
chiragsheta <chiragsheta73@gmail.com>
2022-03-15 17:54:22 +0530

    Update web.rb

--- web.rb
+++ web.rb
@@ -282,7 +282,7 @@ post '/create-customer-new0' do
     @customer = create_customer2(data['name'],data['email'],data['phone'],data['description'],data['my_customer_id'],data['line1'],data['line2'],data['city'],data['state'],data['postal_code'])
   end
   rescue Stripe::InvalidRequestError
- #puts @customer
+ puts @customer
 end
 
 get '/create-customer-new2' do 	
